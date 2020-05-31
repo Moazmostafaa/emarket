3907a4a967bf1f71ebada630db63ca5516668842
Moaz Mostafa <moazmostafa67@outlook.com>
2020-05-31 23:59:10 +0200

    add db validation

---
+++ emarket/Migrations/202005312152050_updatedbValidation.cs
@@ -0,0 +1,24 @@
+﻿namespace emarket.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedbValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Categories", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Image", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "Description", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Products", "Description", c => c.String());
+            AlterColumn("dbo.Products", "Image", c => c.String());
+            AlterColumn("dbo.Products", "Name", c => c.String());
+            AlterColumn("dbo.Categories", "Name", c => c.String());
+        }
+    }
+}
--- emarket/Models/Product.cs
+++ emarket/Models/Product.cs
@@ -10,10 +10,15 @@ namespace emarket.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Price { get; set; }
+        [Required]
         public string Image { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int CategoryId { get; set; }
     }
 }
